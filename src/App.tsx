708456abf618bc44ea52f304a14c7487d9eac9a6
Rafał Chełstowski <rafalchelstowski@Rafas-MacBook-Pro.local>
2021-12-05 09:26:23 +0100

    hdri

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import { Suspense } from 'react';
 
-import { Debug, Physics } from '@react-three/cannon';
-import { Sky, useContextBridge } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
-import { DefaultXRControllers, Hands, VRCanvas } from '@react-three/xr';
+import { Environment, useContextBridge } from '@react-three/drei';
+import { DefaultXRControllers, VRCanvas } from '@react-three/xr';
 import { Route } from 'wouter';
 
 import { OrbitControls } from './common/components/controls/OrbitControls';
-import { Colliders } from './common/components/HandColliders';
 import { Lights } from './common/components/lights/Lights';
-import { Cube } from './features/cube/Cube';
-import { Floor } from './features/floor/Floor';
-import { Rain } from './features/harnasie/Rain';
-import { Kitchen } from './features/kitchen/Kitchen';
-import { Model } from './features/models/001';
+import Kitchen from './features/kitchen/Kitchen';
 
 export function App(): JSX.Element {
   if (!window.ReactQueryClientContext) {
@@ -25,84 +18,15 @@ export function App(): JSX.Element {
   return (
     <main className="w-screen h-screen overflow-hidden">
       <Route path="/">
-        <VRCanvas shadows>
-          <ContextBridge>
-            <Lights />
-            <Cube position={[0, 2, -1.5]} />
-            <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
-              <planeBufferGeometry args={[100, 100]} />
-              <meshStandardMaterial color="white" />
-            </mesh>
-            <DefaultXRControllers />
-            <spotLight castShadow position={[0, 4, 0]} />
-            <OrbitControls />
-          </ContextBridge>
-        </VRCanvas>
-      </Route>
-      <Route path="/physics">
         <VRCanvas>
           <ContextBridge>
             <Lights />
-            <Physics
-              gravity={[0, -2, 0]}
-              iterations={20}
-              defaultContactMaterial={{
-                friction: 0.09,
-              }}
-            >
-              <Rain num={50} />
-              <Debug color="black" scale={1.1}>
-                <Floor position={[0, 1, 0]} />
-                <Hands />
-                <DefaultXRControllers />
-                <Colliders />
-              </Debug>
-            </Physics>
-            <Sky
-              distance={450000}
-              sunPosition={[0, 1, 0]}
-              inclination={0}
-              azimuth={0.25}
-            />
-            <OrbitControls />
-          </ContextBridge>
-        </VRCanvas>
-      </Route>
-      <Route path="/kitchen">
-        <VRCanvas>
-          <ContextBridge>
-            <Lights />
-            <Physics
-              gravity={[0, -2, 0]}
-              iterations={20}
-              defaultContactMaterial={{
-                friction: 0.09,
-              }}
-            >
-              {/* <Debug color="black" scale={1.1}> */}
-              <Floor transparent interactive />
-              <Hands />
-              <DefaultXRControllers />
-              <Colliders />
-              <Suspense fallback={null}>
-                <Kitchen />
-              </Suspense>
-              {/* </Debug> */}
-            </Physics>
-            <OrbitControls />
-          </ContextBridge>
-        </VRCanvas>
-      </Route>
-      <Route path="/playground">
-        <VRCanvas>
-          <ContextBridge>
-            <Lights />
-            {/* <spotLight castShadow position={[0, 10, 0]} /> */}
-            <DefaultXRControllers />
             <Suspense fallback={null}>
-              <Model />
+              <Kitchen />
+              <Environment preset="park" background />
             </Suspense>
             <OrbitControls />
+            <DefaultXRControllers />
           </ContextBridge>
         </VRCanvas>
       </Route>
--- src/common/components/lights/Lights.tsx
+++ src/common/components/lights/Lights.tsx
@@ -1,11 +1,11 @@
-import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 
 import { useThree } from '@react-three/fiber';
 
 export function Lights(): JSX.Element {
   const { gl } = useThree();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     gl.physicallyCorrectLights = true;
   }, [gl]);
 
@@ -17,7 +17,7 @@ export function Lights(): JSX.Element {
       {/* <pointLight position={[1, 2, -5]} intensity={1} power={30} decay={2} /> */}
       {/* <spotLight position={[0, 1.7, 0]} intensity={1} power={10} decay={2} /> */}
       {/* <pointLight position={[-1, 1.7, -5]} decay={2} intensity={1} power={50} /> */}
-      {/* <pointLight position={[1, 1.7, 5]} decay={2} intensity={1} power={50} /> */}
+      {/* <pointLight position={[1,s 1.7, 5]} decay={2} intensity={1} power={50} /> */}
     </>
   );
 }
